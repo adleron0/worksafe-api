66a207f3cc8ec55e85c74b64f9f38664cb976eda
julioa-adl <julioafmesquita@gmail.com>
2025-05-08 09:57:15 -0300

    atualiza consoles conexao main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientInitializationError } from '@prisma/client/runtime/library';
 
 const PORT = process.env.PORT || 3000;
 const ORIGIN_CORS = process.env.ORIGIN_CORS || '*';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ORIGIN_CORS,
-    credentials: true,
-  });
-  const server = await app.listen(PORT);
-  const { port: actualPort } = server.address();
-  console.log(`Application is running on Port ${actualPort}`);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: ORIGIN_CORS,
+      credentials: true,
+    });
+    const server = await app.listen(PORT);
+    const { port: actualPort } = server.address();
+    console.log(`Application is running on Port ${actualPort}`);
+  } catch (error) {
+    console.error('Erro ao inicializar a aplicação:', error);
+    if (error instanceof PrismaClientInitializationError) {
+      console.error('Erro de conexão com o banco de dados:', error.message);
+    } else {
+      console.error('Erro inesperado:', error);
+    }
+  }
 }
 bootstrap();
