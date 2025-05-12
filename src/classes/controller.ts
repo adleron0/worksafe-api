abcaf8d470a204985f91c4ff655f9dfbb5bc2571
julioa-adl <julioafmesquita@gmail.com>
2025-05-12 10:09:54 -0300

    false values controller classes

--- src/classes/controller.ts
+++ src/classes/controller.ts
@@ -91,6 +91,11 @@ export class ClassesController extends GenericController<
     @Body() UpdateDto: UpdateDto,
     @UploadedFile() file?: Express.MulterS3.File,
   ) {
+    if (!UpdateDto.price) UpdateDto.price = null;
+    if (!UpdateDto.oldPrice) UpdateDto.oldPrice = null;
+    if (!UpdateDto.openClass) UpdateDto.openClass = false;
+    if (!UpdateDto.allowExam) UpdateDto.allowExam = false;
+    if (!UpdateDto.allowReview) UpdateDto.allowReview = false;
     return super.update(id, request, UpdateDto, file);
   }
 
