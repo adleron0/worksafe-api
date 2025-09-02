8aae99383275cdfdd0f985f34956224c97074ecb
adleron0 <adleron0@gmail.com>
2025-09-02 13:50:17 -0300

    testa buscar cursos pelo domain

--- src/features/training/course/service.ts
+++ src/features/training/course/service.ts
@@ -28,9 +28,30 @@ export class CourseService extends GenericService<
    */
   async list(search: any, entity: entity) {
     try {
+      let companyId = search.companyId;
+      if (!companyId && search.domain) {
+        const company = await this.prisma.select(
+          'company',
+          {
+            where: {
+              or: [
+                { lp_domain: search.lp_domain },
+                { system_domain: search.system_domain },
+              ],
+            },
+          },
+          null,
+        );
+        if (company.length > 0) {
+          companyId = company[0].id;
+        }
+      }
+      if (!companyId) {
+        return [];
+      }
       const params: any = {
         where: {
-          companyId: Number(search.companyId),
+          companyId: Number(companyId),
           inactiveAt: null,
         },
       };
